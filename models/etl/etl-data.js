bf95a8e9eb046fb74aea8f7f43744c813c85b00b
Michael Chan <mhcchan@ucdavis.edu>
2021-08-10 14:51:33 -0700

    SQL Initialize File

--- models/etl/etl-data.js
+++ models/etl/etl-data.js
@@ -1,25 +1,37 @@
 const path = require('path');
+const fs = require('fs');
 
 const { pool, client } = require('../../helpers/database');
 
 const [,, modeArg, pathArg] = process.argv;
 
 const initializeDatabase = async () => {
-
+  const dataFilePath = path.join(__dirname, 'qa.sql');
+  try {
+    const sqlFileContent = fs.readFileSync(dataFilePath).toString();
+    // console.log(sqlFileContent);
+    console.log('here');
+    const createDatabase = await client.query(sqlFileContent, (err, data) => {
+      console.log(err);
+      console.log(data);
+    });
+    console.log(createDatabase);
+    console.log('finishing');
+    console.log(createDatabase);
+  } catch (e) {
+    console.log(e);
+  }
+  // console.log(sqlFile.toString());
 };
 
 const parseAndInsertData = () => {
 
 };
 
-if (modeArg === 1) {
+if (modeArg === '1') {
   initializeDatabase();
-  //
 } else if (modeArg === 2) {
   parseAndInsertData();
-  //
 }
 
-const dataFilePath = path.join(__dirname, pathArg);
-
-process.stdout.write(`${dataFilePath}\n`);
+// process.stdout.write(`${dataFilePath}\n`);
--- models/etl/qa.sql
+++ models/etl/qa.sql
@@ -5,21 +5,38 @@ DROP TABLE IF EXISTS contentphotos;
 DROP TABLE IF EXISTS photos;
 
 CREATE TABLE questions (
-
+  id SERIAL PRIMARY KEY,
+  helpful INTEGER DEFAULT 0,
+  reported BOOLEAN DEFAULT 'false',
+  body VARCHAR,
+  username VARCHAR(100),
+  email VARCHAR(255),
+  date timestamp
 );
 
 CREATE TABLE answers (
-
+  id SERIAL PRIMARY KEY,
+  helpful INTEGER DEFAULT 0,
+  reported BOOLEAN DEFAULT 'false',
+  body VARCHAR,
+  username VARCHAR(100),
+  date timestamp
 );
 
-CREATE TABLE qanda {
+CREATE TABLE qanda (
+  id SERIAL PRIMARY KEY,
+  question INTEGER NOT NULL,
+  answer INTEGER NOT NULL
+);
 
-};
+CREATE TABLE contentphotos (
+  id SERIAL PRIMARY KEY,
+  question INTEGER,
+  answer INTEGER,
+  photo INTEGER NOT NULL
+);
 
-CREATE TABLE contentphotos {
-
-};
-
-CREATE TABLE photos {
-
-};
+CREATE TABLE photos (
+  id SERIAL PRIMARY KEY,
+  url VARCHAR NOT NULL
+);
