55364040c7183365baa88604ae1ead9cebacf509
Michael Chan <mhcchan@ucdavis.edu>
2021-08-09 19:58:57 -0700

    PostgreSQL Connection Success

--- helpers/database.js
+++ helpers/database.js
@@ -1,14 +1,24 @@
-const pgp = require('pg-promise')();
+const { Pool, Client } = require('pg');
 
-const db = pgp({
+const config = {
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
+};
+
+console.log(config);
+
+const pool = new Pool(config);
+const client = new Client(config);
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res);
+  pool.end();
 });
 
 module.exports = {
-  pgp,
-  db,
+  pool,
+  client,
 };
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 
-const database = require('./helpers/database');
 const app = express();
 const port = process.env.PORT || 3005;
+
+const database = require('./helpers/database');
 
 app.get('/', (req, res) => {
   res.send('helloworld');
 });
+
+
 
 app.listen(port, () => {
   console.log(port);
